70208bab43d8fbb4164863024e7ec7269a3e7351
mkeropian <mkeropian@gmail.com>
2025-07-25 13:21:33 -0300

    25 Jul, integración mapbox, componentes departamentos, fullscreen, cards

---
+++ scripts/set-envs.js
@@ -0,0 +1,23 @@
+const { writeFileSync, mkdirSync } = require('fs');
+
+require ( 'dotenv' ).config();
+
+const targetPath = './src/environments/environment.ts';
+const targetPathDev = './src/environments/environment.development.ts';
+
+const mapboxKey = process.env['MAPBOX_KEY'];
+
+if ( !mapboxKey) {
+  throw new Error('MAPBOX_KEY is not set in .env file')
+}
+
+const envFileContent = `
+export const environment = {
+  MAPBOX_KEY: "${ mapboxKey }",
+};
+`
+
+mkdirSync('./src/environments', { recursive: true });
+
+writeFileSync(targetPath, envFileContent);
+writeFileSync(targetPathDev, envFileContent);
--- src/app/web-front/web-front.route.ts
+++ src/app/web-front/web-front.route.ts
@@ -1,12 +1,14 @@
 import { Routes } from "@angular/router";
-import { HomePageComponent } from "./pages/home-page/home-page/home-page.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { DepartamentosPageComponent } from "./pages/departamentos-page/departamentos-page/departamentos-page.component";
 import { WebFrontComponent } from "./layouts/web-front-layout/web-front/web-front.component";
-import { PropietariosPageComponent } from "./pages/propietarios-page/propietarios-page/propietarios-page.component";
-import { EmpleadosPageComponent } from "./pages/empleados-page/empleados-page/empleados-page.component";
-import { CostosPageComponent } from "./pages/costos-page/costos-page/costos-page.component";
-import { IncomesPageComponent } from "./pages/incomes-page/incomes-page/incomes-page.component";
-import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page/not-found-page.component";
+import { PropietariosPageComponent } from "./pages/propietarios-page/propietarios-page.component";
+import { EmpleadosPageComponent } from "./pages/empleados-page/empleados-page.component";
+import { CostosPageComponent } from "./pages/costos-page/costos-page.component";
+import { IncomesPageComponent } from "./pages/incomes-page/incomes-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+import { FullscreenMapPageComponent } from "./pages/departamentos-page/fullscreen-map-page/fullscreen-map-page.component";
+import { CardsPageComponent } from "./pages/departamentos-page/cards-page/cards-page.component";
 
 export const webFrontRoutes: Routes = [
   {
@@ -21,6 +23,14 @@ export const webFrontRoutes: Routes = [
       {
         path: 'departamentos',
         component: DepartamentosPageComponent,
+      },
+      {
+        path:'fullscreen_map',
+        component: FullscreenMapPageComponent,
+      },
+      {
+        path:'cards',
+        component: CardsPageComponent,
       },
       {
         path: 'propietarios',
