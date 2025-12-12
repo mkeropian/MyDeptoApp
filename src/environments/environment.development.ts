4b942d71d8cc2d0ae6f680595c4bf138ca5c6546
mkeropian <mkeropian@gmail.com>
2025-12-12 16:10:18 -0300

    12 Dic, modifico el frontend para subirlo al hosting,  .env .gitignore angular.json netlify.toml package.json /user-page/edit-modal/edit-modal.component environment.development src/index.html

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost:3050/api',
+  MAPBOX_KEY: "pk.eyJ1IjoibWFyaWFub21hcHMyNTc4IiwiYSI6ImNtYmljYTBybjA0NTgyam9tOTV3bjY3N3QifQ.A5kR8Eg5l2FYEM2qSaSEZg",
+};
