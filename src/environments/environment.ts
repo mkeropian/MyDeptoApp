51782b7c59d900aa0b2177a47fc33a1906c7716c
mkeropian <mkeropian@gmail.com>
2025-12-15 16:14:01 -0300

    15 Dic, Update backend URL para producción.

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+
+// src/environments/environment.ts - PRODUCCIÓN
+
+export const environment = {
+  production: true,
+  baseUrl: 'https://servidorproyectaa-production.up.railway.app/api',
+  MAPBOX_KEY: "pk.eyJ1IjoibWFyaWFub21hcHMyNTc4IiwiYSI6ImNtYmljYTBybjA0NTgyam9tOTV3bjY3N3QifQ.A5kR8Eg5l2FYEM2qSaSEZg",
+};
